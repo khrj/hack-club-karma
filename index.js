19ff5c98de4dd5204dcb0968bda533c7adc09547
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-01-13 20:59:06 +0530

    Add reaction added event

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
 })
 
+app.event("reaction_added", async ({ event, client }) => {
+    console.log(event)
+    if (event.reaction === "upvote") {
+        if (event.user === event.item_user) {
+            client.chat.postEphemeral({
+                channel: event.item.channel,
+                text: "Upvoting / Downvoting your own post does not affect Karma",
+                user: event.user
+            })
+            return
+        }
+        await modifyKarma(1, event.item_user, event.item.channel)
+        console.log(`${event.user} upvoted ${event.item_user}`)
+    } else if (event.reaction === "downvote") {
+        if (event.user === event.item_user) {
+            client.chat.postEphemeral({
+                channel: event.item.channel,
+                text: "Upvoting / Downvoting your own post does not affect Karma",
+                user: event.user
+            })
+            return
+        }
+        await modifyKarma(-1, event.item_user, event.item.channel)
+        console.log(`${event.user} downvoted ${event.item_user}`)
+    }
+})
 async function main() {
     await app.start(process.env.PORT || 3000)
     console.log('⚡️ Bolt app is running!')
