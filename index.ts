4b786942f7d925b21ca625539d83545ab93bbe9f
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-03-30 20:39:36 +0530

    Use TypeScript, reformat

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const { App } = require('@slack/bolt')
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from "@prisma/client"
+import { App } from "@slack/bolt"
 const prisma = new PrismaClient()
 
 const app = new App({
@@ -11,52 +11,57 @@ const admins = [
     "U01C21G88QM", // Khushraj
     "UDFBPS5CZ", // Edwin
     "U013B6CPV62", // Caleb
-    "UARKJATPW" // Claire
+    "UARKJATPW", // Claire
 ]
 
 app.event("reaction_added", async ({ event, client }) => {
-    if (event.reaction === "upvote") {
-        if (event.user === event.item_user) {
-            client.chat.postEphemeral({
-                channel: event.item.channel,
-                text: "Upvoting / Downvoting your own post does not affect Karma",
-                user: event.user
-            })
-            return
+    if ("channel" in event.item) {
+        if (event.reaction === "upvote") {
+            if (event.user === event.item_user) {
+                client.chat.postEphemeral({
+                    channel: event.item.channel,
+                    text: "Upvoting / Downvoting your own post does not affect Karma",
+                    user: event.user,
+                })
+                return
+            }
+            await modifyKarma(1, event.item_user, event.item.channel)
+            console.log(`${event.user} upvoted ${event.item_user}`)
+        } else if (event.reaction === "downvote") {
+            if (event.user === event.item_user) {
+                client.chat.postEphemeral({
+                    channel: event.item.channel,
+                    text: "Upvoting / Downvoting your own post does not affect Karma",
+                    user: event.user,
+                })
+                return
+            }
+            await modifyKarma(-1, event.item_user, event.item.channel)
+            console.log(`${event.user} downvoted ${event.item_user}`)
         }
-        await modifyKarma(1, event.item_user, event.item.channel)
-        console.log(`${event.user} upvoted ${event.item_user}`)
-    } else if (event.reaction === "downvote") {
-        if (event.user === event.item_user) {
-            client.chat.postEphemeral({
-                channel: event.item.channel,
-                text: "Upvoting / Downvoting your own post does not affect Karma",
-                user: event.user
-            })
-            return
-        }
-        await modifyKarma(-1, event.item_user, event.item.channel)
-        console.log(`${event.user} downvoted ${event.item_user}`)
     }
 })
 
 app.event("reaction_removed", async ({ event }) => {
-    if (event.reaction === "upvote") {
-        if (event.user === event.item_user) return
-        await modifyKarma(-1, event.item_user, event.item.channel)
-        console.log(`${event.user} removed upvote from ${event.item_user}`)
-    } else if (event.reaction === "downvote") {
-        if (event.user === event.item_user) return
-        await modifyKarma(1, event.item_user, event.item.channel)
-        console.log(`${event.user} removed downvote from ${event.item_user}`)
+    const channelInEventItem = (item: Record<string, unknown>): item is { channel: string } => "channel" in item
+    if (channelInEventItem(event.item)) {
+        if (event.reaction === "upvote") {
+            if (event.user === event.item_user) return
+            await modifyKarma(-1, event.item_user, event.item.channel)
+            console.log(`${event.user} removed upvote from ${event.item_user}`)
+        } else if (event.reaction === "downvote") {
+            if (event.user === event.item_user) return
+            await modifyKarma(1, event.item_user, event.item.channel)
+            console.log(`${event.user} removed downvote from ${event.item_user}`)
+        }
     }
 })
 
-async function modifyKarma(amount, user, channel) {
+async function modifyKarma(amount: number, user: string, channel: string) {
     const channelObject = await prisma.channel.upsert({
         where: { id: channel },
         create: { id: channel },
-        update: {}
+        update: {},
     })
 
     if (channelObject.enabled) {
@@ -64,30 +69,30 @@ async function modifyKarma(amount, user, channel) {
             where: {
                 id_channelId: {
                     id: user,
-                    channelId: channel
-                }
+                    channelId: channel,
+                },
             },
             create: {
                 id: user,
                 channel: {
                     connect: {
-                        id: channel
-                    }
+                        id: channel,
+                    },
                 },
-                karmaForChannel: amount
+                karmaForChannel: amount,
             },
             update: {
                 karmaForChannel: {
-                    increment: amount
-                }
-            }
+                    increment: amount,
+                },
+            },
         })
     }
 }
 
-app.command('/karma-leaderboard-global', async ({ command, ack, client }) => {
+app.command("/karma-leaderboard-global", async ({ command, ack, client }) => {
     await ack()
-    const map = {}
+    const map: Record<string, number> = {}
     const data = await prisma.user.findMany()
     for (const { id, karmaForChannel } of data) {
         id in map ? map[id] += karmaForChannel : map[id] = karmaForChannel
@@ -104,17 +109,17 @@ app.command('/karma-leaderboard-global', async ({ command, ack, client }) => {
     await client.chat.postEphemeral({
         channel: command.channel_id,
         user: command.user_id,
-        text: build
+        text: build,
     })
 })
 
-app.command('/karma-leaderboard-channel', async ({ command, ack, client }) => {
+app.command("/karma-leaderboard-channel", async ({ command, ack, client }) => {
     await ack()
-    const map = {}
+    const map: Record<string, number> = {}
     const data = await prisma.user.findMany({
         where: {
-            channelId: command.channel_id
-        }
+            channelId: command.channel_id,
+        },
     })
     for (const { id, karmaForChannel } of data) {
         id in map ? map[id] += karmaForChannel : map[id] = karmaForChannel
@@ -131,22 +136,22 @@ app.command('/karma-leaderboard-channel', async ({ command, ack, client }) => {
     await client.chat.postEphemeral({
         channel: command.channel_id,
         user: command.user_id,
-        text: build
+        text: build,
     })
 })
 
-app.command('/karma-global-personal', async ({ command, ack, client }) => {
+app.command("/karma-global-personal", async ({ command, ack, client }) => {
     await ack()
     const karma = await prisma.user.findMany({
         where: {
-            id: command.user_id
+            id: command.user_id,
         },
         select: {
             id: false,
             channelId: false,
             channel: false,
-            karmaForChannel: true
-        }
+            karmaForChannel: true,
+        },
     })
 
     let totalKarma = 0
@@ -157,24 +162,26 @@ app.command('/karma-global-personal', async ({ command, ack, client }) => {
     client.chat.postEphemeral({
         channel: command.channel_id,
         user: command.user_id,
-        text: `You have a total of ${totalKarma} Karma across channels ${getRandomEmoji()}`
+        text: `You have a total of ${totalKarma} Karma across channels ${getRandomEmoji()}`,
     })
 })
 
-app.command('/karma-global-user', async ({ command, ack, client }) => {
+app.command("/karma-global-user", async ({ command, ack, client }) => {
     await ack()
-    const userID = command.text.match(/<@(.*)\|.*>/i)[1]
-    if (userID) {
+
+    const match = command.text.match(/<@(.*)\|.*>/i)
+    if (match) {
+        const userID = match[1]
         const karma = await prisma.user.findMany({
             where: {
-                id: userID
+                id: userID,
             },
             select: {
                 id: false,
                 channelId: false,
                 channel: false,
-                karmaForChannel: true
-            }
+                karmaForChannel: true,
+            },
         })
 
         let totalKarma = 0
@@ -185,93 +192,99 @@ app.command('/karma-global-user', async ({ command, ack, client }) => {
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `<@${userID}> has a total of ${totalKarma} Karma across channels ${getRandomEmoji()}`
+            text: `<@${userID}> has a total of ${totalKarma} Karma across channels ${getRandomEmoji()}`,
         })
     } else {
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `Command parse error. Make sure you include a mention in your command`
+            text: `Command parse error. Make sure you include a mention in your command`,
         })
     }
 })
 
-app.command('/karma-channel-personal', async ({ command, ack, client }) => {
+app.command("/karma-channel-personal", async ({ command, ack, client }) => {
     await ack()
     const karma = await prisma.user.findUnique({
         where: {
             id_channelId: {
                 id: command.user_id,
-                channelId: command.channel_id
-            }
+                channelId: command.channel_id,
+            },
         },
         select: {
             id: false,
             channelId: false,
             channel: false,
-            karmaForChannel: true
-        }
+            karmaForChannel: true,
+        },
     })
 
     client.chat.postEphemeral({
         channel: command.channel_id,
         user: command.user_id,
-        text: `You have a total of ${karma ? karma.karmaForChannel : 0} Karma in <#${command.channel_id}> ${getRandomEmoji()}`
+        text: `You have a total of ${
+            karma ? karma.karmaForChannel : 0
+        } Karma in <#${command.channel_id}> ${getRandomEmoji()}`,
     })
 })
 
-app.command('/karma-channel-user', async ({ command, ack, client }) => {
+app.command("/karma-channel-user", async ({ command, ack, client }) => {
     await ack()
-    const userID = command.text.match(/<@(.*)\|.*>/i)[1]
-    if (userID) {
+    const match = command.text.match(/<@(.*)\|.*>/i)
+    if (match) {
+        const userID = match[1]
         const karma = await prisma.user.findUnique({
             where: {
                 id_channelId: {
                     id: userID,
-                    channelId: command.channel_id
-                }
+                    channelId: command.channel_id,
+                },
             },
             select: {
                 id: false,
                 channelId: false,
                 channel: false,
-                karmaForChannel: true
-            }
+                karmaForChannel: true,
+            },
         })
 
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `<@${userID}> has a total of ${karma ? karma.karmaForChannel : 0} Karma in <#${command.channel_id}> ${getRandomEmoji()}`
+            text: `<@${userID}> has a total of ${
+                karma ? karma.karmaForChannel : 0
+            } Karma in <#${command.channel_id}> ${getRandomEmoji()}`,
         })
     } else {
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `Command parse error. Make sure you include a mention in your command`
+            text: `Command parse error. Make sure you include a mention in your command`,
         })
     }
 })
 
-app.command('/karma-distribution-user', async ({ command, ack, client }) => {
+app.command("/karma-distribution-user", async ({ command, ack, client }) => {
     await ack()
-    const userID = command.text.match(/<@(.*)\|.*>/i)[1]
-    if (userID) {
+    const match = command.text.match(/<@(.*)\|.*>/i)
+    if (match) {
+        const userID = match[1]
         let build = `Distribution of <@${userID}>'s Karma:\n`
 
         const karma = await prisma.user.findMany({
             where: {
-                id: userID
+                id: userID,
             },
             select: {
                 id: false,
                 channelId: true,
                 channel: false,
-                karmaForChannel: true
+                karmaForChannel: true,
             },
             orderBy: {
-                karmaForChannel: 'desc'
-            }
+                karmaForChannel: "desc",
+            },
         })
 
         let totalKarma = 0
@@ -285,36 +298,35 @@ app.command('/karma-distribution-user', async ({ command, ack, client }) => {
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: build
+            text: build,
         })
-
     } else {
         client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `Command parse error. Make sure you include a mention in your command`
+            text: `Command parse error. Make sure you include a mention in your command`,
         })
     }
 })
 
-app.command('/karma-distribution-personal', async ({ command, ack, client }) => {
+app.command("/karma-distribution-personal", async ({ command, ack, client }) => {
     await ack()
 
     let build = "Distribution of your Karma:\n"
 
     const karma = await prisma.user.findMany({
         where: {
-            id: command.user_id
+            id: command.user_id,
         },
         select: {
             id: false,
             channelId: true,
             channel: false,
-            karmaForChannel: true
+            karmaForChannel: true,
         },
         orderBy: {
-            karmaForChannel: 'desc'
-        }
+            karmaForChannel: "desc",
+        },
     })
 
     let totalKarma = 0
@@ -328,80 +340,80 @@ app.command('/karma-distribution-personal', async ({ command, ack, client }) => 
     client.chat.postEphemeral({
         channel: command.channel_id,
         user: command.user_id,
-        text: build
+        text: build,
     })
 })
 
-app.command('/karma-enable', async ({ command, ack, client }) => {
+app.command("/karma-enable", async ({ command, ack, client }) => {
     await ack()
     if (admins.includes(command.user_id)) {
         await prisma.channel.upsert({
             where: { id: command.channel_id },
             create: {
                 id: command.channel_id,
-                enabled: true
+                enabled: true,
             },
-            update: { enabled: true }
+            update: { enabled: true },
         })
         try {
             await client.conversations.join({
-                channel: command.channel_id
+                channel: command.channel_id,
             })
         } catch {
-            // Already in channel 
+            // Already in channel
         }
         await client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `Enabled Karma for <#${command.channel_id}>`
+            text: `Enabled Karma for <#${command.channel_id}>`,
         })
     } else {
         await client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: "Unfortunately, you don't have authorization to do that"
+            text: "Unfortunately, you don't have authorization to do that",
         })
     }
 })
 
-app.command('/karma-disable', async ({ command, ack, client }) => {
+app.command("/karma-disable", async ({ command, ack, client }) => {
     await ack()
     if (admins.includes(command.user_id)) {
         await prisma.channel.upsert({
             where: { id: command.channel_id },
             create: {
                 id: command.channel_id,
-                enabled: false
+                enabled: false,
             },
-            update: { enabled: false }
+            update: { enabled: false },
         })
         await client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: `Disabled Karma for <#${command.channel_id}>`
+            text: `Disabled Karma for <#${command.channel_id}>`,
         })
     } else {
         await client.chat.postEphemeral({
             channel: command.channel_id,
             user: command.user_id,
-            text: "Unfortunately, you don't have authorization to do that"
+            text: "Unfortunately, you don't have authorization to do that",
         })
     }
 })
 
 async function main() {
     await app.start(process.env.PORT || 3000)
-    console.log('⚡️ Bolt app is running!')
+    console.log("⚡️ Bolt app is running!")
 }
 
 main()
 
 function getRandomEmoji() {
     const emojis = [
-        ':sunglasses:',
-        ':parrot:',
-        ':yay:',
-        ':eyes:'
+        ":sunglasses:",
+        ":parrot:",
+        ":yay:",
+        ":eyes:",
     ]
     return emojis[Math.floor(Math.random() * emojis.length)]
 }
